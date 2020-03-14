9f4e45f44cc4a22beb7734b87edccc5daf63fbf9
Amit <amitmoryossef@gmail.com>
2020-03-14 11:30:19 +0200

    feat(performance): add sequence of performance tests

--- sample-data/performance.py
+++ sample-data/performance.py
@@ -1,33 +1,37 @@
+import struct
+
 import imgaug.augmenters as iaa
 import imgaug as ia
-
+import numpy as np
 from tqdm import tqdm
 from lib.python.pose_format import PoseReader, Pose
-from lib.python.pose_format.custom_augment.piecewise_affine_kp import PiecewiseAffineKP
+
+iterations = 100
 
 buffer = open("1.pose", "rb").read()
 p = PoseReader(buffer).read()
 
-for _ in tqdm(list(range(10))):
+print("Unpack file")
+for _ in tqdm(range(iterations), total=iterations):
+    p = PoseReader(buffer).read()
+
+print("Focus Pose")
+for _ in tqdm(range(iterations), total=iterations):
+    p.focus_pose()
+
+print("Normalize")
+for _ in tqdm(range(iterations), total=iterations):
     p.normalize(
         dist_p1=("pose_keypoints_2d", 2),  # RShoulder
         dist_p2=("pose_keypoints_2d", 5),  # LShoulder
     )
 
-for _ in tqdm(list(range(10))):
+print("Vectorize")
+for _ in tqdm(range(iterations), total=iterations):
     list(p.to_vectors(["angle", "distance"]))
 
-seq = iaa.Sequential([
-    iaa.HorizontalFlip(0.5),  # 50% of poses should be flipped left/right
-    PiecewiseAffineKP(scale=(0.01, 0.05)),  # Distort keypoints
-    iaa.Affine(
-        rotate=(-5, 5),  # Rotate up to 10 degrees each way
-        scale={"x": (0.9, 1.1), "y": (0.9, 1.1)},  # Stretch or squash up to 50% each direction independently
-        shear={"x": (-10, 10), "y": (-10, 10)}  # Shear X/Y up to 16 degrees independently
-    ),
-    iaa.PerspectiveTransform(scale=(0.0, 0.1))  # Change perspective
-])
 
-for _ in tqdm(list(range(10))):
+print("Augment")
+for _ in tqdm(range(iterations), total=iterations):
+    seq = iaa.Sequential([])
     p.augment2d(seq)
-

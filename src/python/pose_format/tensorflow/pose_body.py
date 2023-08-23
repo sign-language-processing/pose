8f532d113b89ed66d604999e3f37ee828ce79e64
rfahrn <58358083+rfahrn@users.noreply.github.com>
2023-08-23 10:54:15 +0200

    add ignore

--- src/python/pose_format/tensorflow/pose_body.py
+++ src/python/pose_format/tensorflow/pose_body.py
@@ -14,9 +14,29 @@ TF_POSE_RECORD_DESCRIPTION = {
 
 
 class TensorflowPoseBody(PoseBody):
+    """
+    Representation of pose body data, optimized for TensorFlow operations.
+
+    * Inherites from PoseBody 
+
+    Parameters
+    ----------
+    fps : float
+        The frames per second for the pose data.
+    data : Union[:class:`~pose_format.tensorflow.masked.tensor.MaskedTensor`, tf.Tensor]
+        The pose data.
+    confidence : tf.Tensor
+        The confidence scores for the pose data.
+    """
+
     tensor_reader = 'unpack_tensorflow'
+    """used to read the tensor data. (Type: str)"""
 
     def __init__(self, fps: float, data: Union[MaskedTensor, tf.Tensor], confidence: tf.Tensor):
+        """
+        initializes the TensorflowPoseBody with fps, data and confidence.
+
+        """
         if isinstance(data, tf.Tensor):  # If array is not masked
             mask = confidence > 0
             data = MaskedTensor(data, tf.stack([mask] * 2, axis=3))
@@ -24,20 +44,41 @@ class TensorflowPoseBody(PoseBody):
         super().__init__(fps, data, confidence)
 
     def zero_filled(self):
+        """Return an instance with zero-filled data."""
         self.data = self.data.zero_filled()
         return self
 
     def select_frames(self, frame_indexes: List[int]):
+        """
+        Selects and returns a subset of frames based on the frame indexes.
+
+        Parameters
+        ----------
+        frame_indexes : List[int]
+            List of frame indexes
+
+        Returns
+        -------
+        TensorflowPoseBody
+            Instance with the selected frames
+        """
         data = self.data.gather(frame_indexes)
         confidence = tf.gather(self.confidence, frame_indexes)
         return self.__class__(fps=self.fps, data=data, confidence=confidence)
 
     def frame_dropout_given_percent(self, dropout_percent: float):
         """
-        Remove some frames from the data at random.
+        Remove some frames from the data at random from pose data.
 
-        :param dropout_percent:
-        :return:
+        Parameters
+        ----------
+        dropout_percent : float
+            The percentage of frames to drop.
+
+        Returns
+        -------
+        TensorflowPoseBody, tf.Tensor
+            A new instance with dropped frames and the selected frame indexes.
         """
         data_len = tf.cast(tf.shape(self.data.tensor)[0], dtype=tf.float32)
 
@@ -58,7 +99,20 @@ class TensorflowPoseBody(PoseBody):
     def frame_dropout_uniform(self,
                               dropout_min: float = 0.2,
                               dropout_max: float = 1.0):
+        """Drops randomly frames based on a given uniform distribution
+        
+        Parameters
+        ----------
+        dropout_min : float, optional
+            minimum percentage for dropout, by default 0.2.
+        dropout_max : float, optional
+            maximum percentage for dropout, by default 1.0.
 
+        Returns
+        -------
+        TensorflowPoseBody
+            Instance with frames dropped based on a uniform distribution.
+        """
         dropout_percent = tf.random.uniform([1], minval=dropout_min, maxval=dropout_max)[0]
 
         return self.frame_dropout_given_percent(dropout_percent)
@@ -66,6 +120,21 @@ class TensorflowPoseBody(PoseBody):
     def frame_dropout_normal(self,
                              dropout_mean: float = 0.5,
                              dropout_std: float = 0.1):
+        """
+        Given mean and standard deviation, randomly drops out based on normal distribution. 
+
+        Parameters
+        ----------
+        dropout_mean : float, optional
+            The mean for the normal distribution, by default 0.5.
+        dropout_std : float, optional
+            The standard deviation for the normal distribution, by default 0.1.
+
+        Returns
+        -------
+        TensorflowPoseBody
+            instance with frames dropped based on normal distribution.
+        """
 
         dropout_percent = tf.random.normal([1], mean=dropout_mean, stddev=dropout_std)[0]
 
@@ -75,9 +144,29 @@ class TensorflowPoseBody(PoseBody):
         return self.frame_dropout_given_percent(dropout_percent)
 
     def points_perspective(self) -> MaskedTensor:
+        """
+        Returns perspective transformation of pose points.
+
+        Returns
+        -------
+        :class:`~pose_format.tensorflow.masked.tensor.MaskedTensor`
+            Transformed pose data.
+        """
         return self.data.transpose(perm=POINTS_DIMS)
 
     def get_points(self, indexes: List[int]):
+        """Gets and returns points from pose data based on indexes 
+
+        Parameters
+        ----------
+        indexes : List[int]
+            List of point indexes to get.
+
+        Returns
+        -------
+        TensorflowPoseBody
+            Instance containing only the gotten points.
+        """
         data = self.data.transpose(perm=POINTS_DIMS)
         new_data = data[indexes].transpose(perm=POINTS_DIMS)
 
@@ -88,6 +177,19 @@ class TensorflowPoseBody(PoseBody):
         return TensorflowPoseBody(self.fps, new_data, new_confidence)
 
     def matmul(self, matrix: np.ndarray) -> __qualname__:
+        """
+        multiplies pose data with a given matrix.
+
+        Parameters
+        ----------
+        matrix : np.ndarray
+            Matrix to multiply with pose data.
+
+        Returns
+        -------
+        TensorflowPoseBody
+            Instance with the pose data multiplied by the matrix.
+        """
         matrix = tf.convert_to_tensor(matrix, dtype=self.data.dtype)
         data = self.data.matmul(matrix)
         return self.__class__(fps=self.fps, data=data, confidence=self.confidence)

d0ac8f47ad53c45118a4d9e078625c6a2aef2ce6
Amit Moryossef <amitmoryossef@gmail.com>
2024-06-25 10:06:18 +0200

    fix(holistic): make sure holistic is closed in case of a failure (fixes #96)

--- src/python/pose_format/utils/holistic.py
+++ src/python/pose_format/utils/holistic.py
@@ -182,42 +182,43 @@ def process_holistic(frames: list,
     """
     holistic = mp_holistic.Holistic(static_image_mode=False, **additional_holistic_config)
 
-    datas = []
-    confs = []
+    try:
+        datas = []
+        confs = []
 
-    for i, frame in enumerate(tqdm(frames, disable=not progress)):
-        results = holistic.process(frame)
+        for i, frame in enumerate(tqdm(frames, disable=not progress)):
+            results = holistic.process(frame)
 
-        body_data, body_confidence = body_points(results.pose_landmarks, w, h, 33)
-        face_data, face_confidence = component_points(results.face_landmarks, w, h,
-                                                      FACE_POINTS_NUM(additional_face_points))
-        lh_data, lh_confidence = component_points(results.left_hand_landmarks, w, h, 21)
-        rh_data, rh_confidence = component_points(results.right_hand_landmarks, w, h, 21)
-        body_world_data, body_world_confidence = body_points(results.pose_world_landmarks, w, h, 33)
+            body_data, body_confidence = body_points(results.pose_landmarks, w, h, 33)
+            face_data, face_confidence = component_points(results.face_landmarks, w, h,
+                                                          FACE_POINTS_NUM(additional_face_points))
+            lh_data, lh_confidence = component_points(results.left_hand_landmarks, w, h, 21)
+            rh_data, rh_confidence = component_points(results.right_hand_landmarks, w, h, 21)
+            body_world_data, body_world_confidence = body_points(results.pose_world_landmarks, w, h, 33)
 
-        data = np.concatenate([body_data, face_data, lh_data, rh_data, body_world_data])
-        conf = np.concatenate([body_confidence, face_confidence, lh_confidence, rh_confidence, body_world_confidence])
+            data = np.concatenate([body_data, face_data, lh_data, rh_data, body_world_data])
+            conf = np.concatenate([body_confidence, face_confidence, lh_confidence, rh_confidence, body_world_confidence])
 
-        if kinect is not None:
-            kinect_depth = []
-            for x, y, z in np.array(data, dtype="int32"):
-                if 0 < x < w and 0 < y < h:
-                    kinect_depth.append(kinect[i, y, x, 0])
-                else:
-                    kinect_depth.append(0)
+            if kinect is not None:
+                kinect_depth = []
+                for x, y, z in np.array(data, dtype="int32"):
+                    if 0 < x < w and 0 < y < h:
+                        kinect_depth.append(kinect[i, y, x, 0])
+                    else:
+                        kinect_depth.append(0)
 
-            kinect_vec = np.expand_dims(np.array(kinect_depth), axis=-1)
-            data = np.concatenate([data, kinect_vec], axis=-1)
+                kinect_vec = np.expand_dims(np.array(kinect_depth), axis=-1)
+                data = np.concatenate([data, kinect_vec], axis=-1)
 
-        datas.append(data)
-        confs.append(conf)
+            datas.append(data)
+            confs.append(conf)
 
-    pose_body_data = np.expand_dims(np.stack(datas), axis=1)
-    pose_body_conf = np.expand_dims(np.stack(confs), axis=1)
+        pose_body_data = np.expand_dims(np.stack(datas), axis=1)
+        pose_body_conf = np.expand_dims(np.stack(confs), axis=1)
 
-    holistic.close()
-
-    return NumPyPoseBody(data=pose_body_data, confidence=pose_body_conf, fps=fps)
+        return NumPyPoseBody(data=pose_body_data, confidence=pose_body_conf, fps=fps)
+    finally:
+        holistic.close()
 
 
 def holistic_hand_component(name, pf="XYZC") -> PoseHeaderComponent:

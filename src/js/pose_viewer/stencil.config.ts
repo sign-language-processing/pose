216f9e6e19b076a3e32ddcf92b3b83fe40cc3f26
Bipin <bipinkrish4@gmail.com>
2024-09-30 20:02:17 +0530

    support to export as custom element

--- src/js/pose_viewer/stencil.config.ts
+++ src/js/pose_viewer/stencil.config.ts
@@ -18,6 +18,10 @@ export const config: Config = {
     {
       type: 'www',
       serviceWorker: null // disable service workers
+    },
+    { 
+      type: 'dist-custom-elements', 
+      externalRuntime: false 
     }
   ]
 };
